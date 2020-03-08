e2cd72c08d4932b6375ead9808dc85dcc93e4e22
Ilham Zannuary <ilhamsudiyono29@gmail.com>
2020-03-08 19:07:02 +0700

    welcome devOps

--- src/main/java/com/helloworld/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/helloworld/helloworld/controller/HelloWorldController.java
@@ -8,7 +8,7 @@ public class HelloWorldController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Welcome devOps!";
     }
 
 }
